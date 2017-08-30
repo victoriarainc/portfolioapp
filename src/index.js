318731f819613d7daeb2c9febd92e39d8e03280a
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-30 09:31:55 -0400

    added and imported React Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
