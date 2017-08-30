284799c1314a46500586e30a523b12eb499fe643
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-30 14:32:14 -0400

    included notes for BaseLayout and imported into App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import BaseLayout from './BaseLayout.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <BaseLayout />
       </div>
     );
   }
--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,1 +1,17 @@
 import React, { Component } from 'react';
+
+export default class BaseLayout extends Component {
+  constructor () {
+    super ()
+
+  }
+  render () {
+    return (
+      <div>
+        <header> Nav links to Home, About, and Portfolio </header>
+        <section>  </section>
+        <footer> Contact Info </footer>
+      </div>
+    )
+  }
+}
