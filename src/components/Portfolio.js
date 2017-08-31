7493b4c30d3cf9701f13e7eb7e1afe6fc58d5358
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-30 21:06:03 -0400

    added content to Portfolio and adjusted global styling -need to fix active and hover states of header and footer

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,12 +8,36 @@ export default class Portfolio extends Component {
     return (
       <section className="portfolio">
         <div>
-      
+
         </div>
 
-        <div>
-        <h2>Victoria Rain</h2>
-        <p> Portfolio </p>
+        <div className="resume">
+          <h3> Core Competencies </h3>
+            <section className="bullet">
+             <p className="line">
+             Git & GitHub - HTML/CSS - JavaScript - React - Node - Mongo/Mongoose - SQL/Postgres - jQuery - Bootstrap
+             </p>
+            </section>
+
+          <h3> Experience </h3>
+            <p className="bullet">
+            My background is varied from customer service and childcare to crafts
+            and demonstrations. I graduated with an Associates in Psychology, my
+            underlying passion that drives all of my pursuits, and Honors credit
+            in the Social Sciences.
+            </p>
+
+          <h3> Projects </h3>
+            <p className="bullet">
+            Follow the links below to view some of my ongoing projects. Instagram
+            will lead you to HandmadebyVi - a crafty wonderland with a focus on
+            paper, recycling, and reuse. Patreon is a related project. It is a
+            site that connects artists to fans and provides a kickstarter/subscription
+            box type platform for any type of creator to post to. ViPhotography
+            is a Facebook page that I manage and post albums of candid portraits to.
+
+            </p>
+
         </div>
       </section>
     )
