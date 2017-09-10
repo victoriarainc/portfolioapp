69fd9d404a1994b4c6d1f17356f4d903c3247e88
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-09-10 15:18:12 -0400

    cleaning up code and added headshot image

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 // import BaseLayout from './BaseLayout.js';
 import '../styles/App.css';
 
+import '../images/vi.jpg'
+
 class App extends Component {
   render() {
     return (
-      <h1> Hi! I'm Victoria. Click on one of the links above to 
-      learn a little more about me! </h1>
+      <div>
+      <img src="../images/vi.jpg" />
+      <h1> Hi! I'm Victoria. </h1>
+      <p>
+      Click on one of the links above to
+      learn a little more about me! </p>
+      </div>
     );
   }
 }
--- src/components/References.js
+++ src/components/References.js
@@ -12,7 +12,6 @@ export default class References extends Component {
           <h2>Stacey Vernon</h2>
           <p>Campus director at The Iron Yard in Raleigh.</p>
           <a href="https://www.linkedin.com/in/staceyvernon/">LinkedIn</a>
-          <p>Phone: 919.867.5309</p>
           <p>Email: stacey@vernon.com</p>
           <blockquote>"Sleep on keyboard meowwww jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed or paw at beetle and eat it before it gets away."</blockquote>
         </div>
@@ -21,7 +20,6 @@ export default class References extends Component {
           <h2>Doug Hughes</h2>
           <p>Instructor and Full Stack Developer at The Iron Yard in Raleigh.</p>
           <a href="https://www.linkedin.com/in/doug-hughes/">LinkedIn</a>
-          <p>Phone: 919.867.5309</p>
           <p>Email: doug@hughes.com</p>
           <blockquote>"Dream about hunting birds knock over christmas tree spend all night ensuring people don't sleep yet chase the pig around the house."</blockquote>
         </div>
@@ -30,25 +28,22 @@ export default class References extends Component {
           <h2>Lexi Namer</h2>
           <p>Instructor and Front End Developer at The Iron Yard in Raleigh.</p>
           <a href="https://www.linkedin.com/in/lexinamer/">LinkedIn</a>
-          <p>Phone: 919.867.5309</p>
-          <p>Email: lexi@namer.com</p>
           <blockquote>"Play riveting piece on synthesizer keyboard lie in the sink all day."</blockquote>
         </div>
 
         <div className="ref">
-          <h2>Stefanie Despagni</h2>
-          <p>Manager at Paper Source in Raleigh.</p>
-          <p>Phone: 919.867.5309</p>
-          <p>Email: stefanie@despagni.com</p>
-          <blockquote>"Scratch at fleas, meow until belly rubs, hide behind curtain when vacuum cleaner is on scratch strangers and poo on owners food."</blockquote>
+          <h2>Kaitlin Saunders</h2>
+          <p>Student Instructor at Iron Yard in Raleigh.</p>
+          <a href='https://www.linkedin.com/in/kaitlin-saunders/'>LinkedIn</a>
+          <blockquote>"Victoria is an excellent team player with strong communication skills and a great attitude. She picks up new, complex concepts quickly and consistently delivers high quality work. No matter the tools or the task, Victoria will find a way to get the job done. "</blockquote>
         </div>
 
         <div className="ref">
-          <h2>Amy Miller</h2>
-          <p>Supervisor at The Dollar Book Exchange in Raleigh.</p>
-          <p>Phone: 919.867.5309</p>
-          <p>Email: amy.miller.com</p>
-          <blockquote>"Roll on the floor purring your whiskers off lick plastic bags, yet have my breakfast spaghetti yarn, claw drapes, but swat at dog. Instead of drinking water from the cat bowl, make sure to steal water from the toilet."</blockquote>
+          <h2>Kelly Miller</h2>
+          <p>Iron Yard - Raleigh</p>
+          <p>Email: ukmills12@gmail.com</p>
+          <blockquote>" Victoria is an excellent problem solver and has a relentless work-ethic. If she does not know the immediate solution to a problem, she will seek the appropriate resources needed to solve it.
+"</blockquote>
         </div>
 
       </section>
